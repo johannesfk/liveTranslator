0788c9db2576384d797f32443b039e191bd3c5cb
Johannes Knudsen <johannesfknudsen@gmail.com>
2019-02-12 15:46:23 +0100

    Google translate
    Added first half iteration of google cloud translate api.

---
+++ docs.md
@@ -0,0 +1,6 @@
+Google Cloud auth: https://cloud.google.com/docs/authentication/getting-started
+
+API Guide: https://cloud.google.com/translate/docs/translating-text
+  Working example:
+https://cloud.google.com/nodejs/docs/reference/translate/2.1.x/
+https://cloud.google.com/translate/docs/quickstart-client-libraries
--- server_yandex.js
+++ server_yandex.js
@@ -2,14 +2,7 @@ require('dotenv').config()
 const axios = require('axios')
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const {Translate} = require('@google-cloud/translate');
-const projectId = 'bothosting';
 // const keep_alive = require('./keep_alive.js')
-
-// Instantiates a translate client
-const translate = new Translate({
-  projectId: projectId,
-});
 
 client.on('message', msg => {
   axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate', {
@@ -18,13 +11,8 @@ client.on('message', msg => {
       text: msg.content,
       lang: 'en'
     }
-  // The text to translate
-  var text = msg.content;
-
-  // The target language
-  const target = 'ru';
   }).then(res => {
-    if (text[0] !== msg.content) {
+    if (res.data.text[0] !== msg.content) {
       msg.reply("In English it is:\n" + res.data.text[0])
       console.log("Translated " + msg.content + " to " + res.data.text[0]);
     }
